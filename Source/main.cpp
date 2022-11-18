daf6a41f92dc63842068dc56c4bb825bc1b1df83
kurciqs <90471896+kurciqs@users.noreply.github.com>
2022-11-18 23:14:25 +0100

    testing git

--- Source/main.cpp
+++ Source/main.cpp
@@ -4,12 +4,18 @@
 int main() {
     chdir("../..");
     SauronLT::Init(1280, 720, "rtx");
+    SauronLT::SetClearColor({0.6f, 0.55f, 0.75f, 1.0f});
 
     while (SauronLT::Running()) {
         SauronLT::BeginFrame();
 
-        SauronLT::SetClearColor({0.6f, 0.55f, 0.75f, 1.0f});
         ImGui::ShowDemoWindow();
+
+        ImGui::Begin("Viewport");
+        {
+            // TODO
+        }
+        ImGui::End();
 
         SauronLT::EndFrame();
     }
